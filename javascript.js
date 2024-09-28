15bfcecd17fbf1003536ca77aafff5cc6c661112
JamieHerron <jamiealex.jh@gmail.com>
2024-09-28 19:27:14 +0800

    functions added to play round and game
    Extra functions added to play one round of the game
    score variables added and a play game function was added
    to iterate through the playRound function 5 times
    declaring a winner at the end

--- javascript.js
+++ javascript.js
@@ -26,3 +26,52 @@ function getHumanChoice() {
         return "You did not choose one of the three options";
     }
 }
+
+/*play game function*/ 
+
+function playGame () {
+
+    let computerScore = 0;
+    let humanScore = 0;
+
+    function playRound (humanChoice, computerChoice) {
+
+        if (humanChoice === "rock" && computerChoice === "paper") {
+            console.log("You lose, paper beats rock")
+            computerScore++
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            console.log("You win, paper beats rock")
+            humanScore++
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            console.log("You win, scissors beat paper")
+            humanScore++
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            console.log("You lose, scissors beat paper")
+            computerScore++
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            console.log("You win, rock beats scissors")
+            humanScore++
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            console.log("You lose, rock beats scissors")
+            computerScore++
+        } else {
+            console.log("This round is a draw")
+        }
+    
+    }
+
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+
+    if (humanScore > computerScore) {
+        console.log(`You win with a score of ${humanScore}. The computer scored ${computerScore}`)
+    } else if (humanScore < computerScore) {
+        console.log(`You lost with a score of ${humanScore}. The computer scored ${computerScore}`)
+    } else {
+        console.log(`This game was a draw with you scoring ${humanScore} and the computer scoring ${computerScore}`)
+    }
+}
+
